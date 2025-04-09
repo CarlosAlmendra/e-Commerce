5d6fadd61320e9f36a143c10ca865d8eadceb020
Carlos <almendracarloshenrique@gmail.com>
2025-04-08 23:59:15 -0300

    add entity user

---
+++ src/main/java/org/unicesumar/entity/User.java
@@ -0,0 +1,28 @@
+package org.unicesumar.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+
+}
