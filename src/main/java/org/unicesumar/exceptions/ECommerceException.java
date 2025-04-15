61d74135b7bd277f952bf7ce2b47f7cf47344e6b
Carlos <almendracarloshenrique@gmail.com>
2025-04-15 20:38:43 -0300

    added custom exception

---
+++ src/main/java/org/unicesumar/exceptions/ECommerceException.java
@@ -0,0 +1,7 @@
+package org.unicesumar.exceptions;
+
+public class ECommerceException extends RuntimeException {
+    public ECommerceException(String message, String cause) {
+        super(message);
+    }
+}
